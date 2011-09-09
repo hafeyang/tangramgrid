5e75e10135a3ce849958709c9c4e8037e6bc7b4e
hafeyang <yanghengfeng@163.com>
2011-09-09 16:52:26 +0800

    修复bug: 数据区不横向溢出表头仍空出纵向滚动条宽度

--- tangramgrid/tangram/tangram.grid.js
+++ tangramgrid/tangram/tangram.grid.js
@@ -66,15 +66,14 @@ baidu.ui.Grid = baidu.ui.createUI(function(opt){
 	 * 计算自适应列的宽度
 	 */
 	_fixColWidth:function(){
-		var g= this,fixColIndex=g.fixColIndex;
-		var s=g.ref.gheader.style,fixcol=null,
-		hdContentWidth = g.ref.gheader.parentNode.offsetWidth-parseInt(baidu.dom.getStyle(g.ref.gheader,"border-left-width"),10)-parseInt(baidu.dom.getStyle(g.ref.gheader,"border-right-width"),10),
+		var g= this,fixColIndex=g.fixColIndex,
+			t=baidu.dom.query(".gridtable",g.ref.gbody),
+			y=parseInt(baidu.dom.getStyle(g.ref.gbody.parentNode,"height"),10)||g.ref.gbody.parentNode.clientHeight,
+			s=g.ref.gheader.style,fixcol=null,
+			hdContentWidth = g.ref.gheader.parentNode.offsetWidth-parseInt(baidu.dom.getStyle(g.ref.gheader,"border-left-width"),10)-parseInt(baidu.dom.getStyle(g.ref.gheader,"border-right-width"),10),
 		w=hdContentWidth;
 		if(fixColIndex!=-1){
 			var t = baidu.dom.query(".gridtable",g.ref.gbody);
-			/*if (t.length> 0) {
-				t[0].style.width=g.ref.gheader.childNodes[0].clientWidth+"px";
-			}*/
 			for (i = 0, l = g.ref.ghcells.length; i < l; i++) {
 				var c=g.ref.ghcells[i],refcol=c.getAttribute("refcol"); 
 				if ((!g.columns[refcol].fix) || (!refcol)) {
@@ -85,8 +84,9 @@ baidu.ui.Grid = baidu.ui.createUI(function(opt){
 			}
 			if (fixcol && w>20) {
 				w= w- parseInt(baidu.dom.getStyle(fixcol,"border-left-width"),10)- parseInt(baidu.dom.getStyle(fixcol,"border-right-width"),10);
-				//baidu.dom.setStyle(fixcol,"width",w);
-				w=w-17;
+				if (t.length > 0 && t[0].offsetHeight > y && (!g.autoHeight)) {
+					w=w-17;
+				}
 				baidu.dom.setBorderBoxWidth(fixcol,w);
 				g.columns[g.fixColIndex].width = w;
 				if(g.ref.gbody){
